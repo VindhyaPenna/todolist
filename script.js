1275c95f1369ad0912066d3b333557242019d9d7
VindhyaPenna <vindhyapenna@gmail.com>
2024-01-09 22:01:34 +0530

    com

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ let delbtn =document.createElement("button");
 task.appendChild(editbtn);
 editbtn.innerHTML = "EDIT"
 delbtn.classList.add("delbtn");
-delbtn.classList.add("delbtn"+i++)
+// delbtn.classList.add("delbtn"+i++)
 delbtn.innerHTML = "DELETE"
 btndiv.appendChild(editbtn);
 btndiv.appendChild(delbtn);
@@ -109,7 +109,15 @@ body.appendChild(taskcontainer)
 // let buttons = document.query 
 taskcontainer.addEventListener('click',(e)=>{
    if (e.target.classList.contains('delbtn')) {
-      e.target.parentElement.parentElement.remove();}
+      // console.log( e.target.parentElement.parentElement.firstChild.innerText);
+      for(let i = 0;i<taskarr.length;i++){
+         // console.log(taskarr[i].task);
+      if(taskarr[i].task == e.target.parentElement.parentElement.firstChild.innerText){
+         taskarr.splice(i,1);
+         // console.log(taskarr);
+      }}
+      e.target.parentElement.parentElement.remove();
+   }
    else if (e.target.classList.contains('editbtn')) {
       // console.log(e.target);
       editTask(e.target);
